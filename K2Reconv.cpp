f3e46d8175b71d0b2f7a84ab95e7fad220942277
Egtra <yusuke.ichinohe@gmail.com>
2012-04-28 20:10:39 +0900

    Bug fix

--- K2Reconv.cpp
+++ K2Reconv.cpp
@@ -35,46 +35,64 @@ WNDPROC oldWindowProc;
 LRESULT OnImeRequestReconvertString(HWND hwnd, RECONVERTSTRING* prcs)
 {
 	DWORD first = 0, last = 0;
-	CallWindowProc(oldWindowProc, hwnd, EM_GETSEL, reinterpret_cast<WPARAM>(&first), reinterpret_cast<LPARAM>(&last));
+	SendMessage(hwnd, EM_GETSEL, reinterpret_cast<WPARAM>(&first), reinterpret_cast<LPARAM>(&last));
 	if (first > last)
 	{
 		std::swap(first, last);
 	}
 
 	//選択範囲（キャレット）を含む行を探す（その行にある文字列すべてを得るため）
-	LRESULT lineStartPos = CallWindowProc(oldWindowProc, hwnd, EM_LINEFROMCHAR, first, 0);
+	LRESULT lineNumberOfStartPos = Edit_LineFromChar(hwnd, first);
 
-	//行末を探す
-	WORD lineLength = static_cast<WORD>(std::max(
-		static_cast<LPARAM>(std::numeric_limits<WORD>::max()),
-		CallWindowProc(oldWindowProc, hwnd, EM_LINELENGTH, lineStartPos, 0)));
+	//行末を探す（EM_GETLINEがWORDで文字列指定するため、WORDの最大値を上限としている）
+	WORD lineLength = static_cast<WORD>(std::min(
+		static_cast<int>(std::numeric_limits<WORD>::max()),
+		Edit_LineLength(hwnd, lineNumberOfStartPos)));
+
+	if (lineLength == 0)
+	{
+		return 0;
+	}
+
 	if (static_cast<LONG>(last - first) > lineLength)
 	{
 		last = first + lineLength;
-		CallWindowProc(oldWindowProc, hwnd, EM_SETSEL, first, last);
+		Edit_SetSel(hwnd, first, last);
 	}
-	DWORD selStrLen = last - first; //選択部分の長さ（選択範囲がなければそのまま0で良い）
-	LRESULT lr = sizeof (RECONVERTSTRING) + selStrLen + 1;
+	DWORD selectedLength = last - first; //選択部分の長さ（選択範囲がなければそのまま0で良い）
+	LRESULT lr = sizeof (RECONVERTSTRING) + lineLength + 1;
 	if (prcs != nullptr)
 	{
 		std::vector<TCHAR> buf(lineLength);
-		reinterpret_cast<WORD&>(buf[0]) = selStrLen;
-		LRESULT bufLen = CallWindowProc(oldWindowProc, hwnd, EM_GETLINE, lineStartPos, reinterpret_cast<LPARAM>(&buf[0]));
+		LRESULT bufLen = Edit_GetLine(hwnd, lineNumberOfStartPos, buf.data(), lineLength);
 		buf.resize(static_cast<std::size_t>(bufLen));
 
 		//必要な情報の設定
 		//1行分の文字列と変換部分のオフセットを渡すようにしている
 		PTSTR dst = reinterpret_cast<PTSTR>(&prcs[1]);
-		std::copy(buf.begin(), buf.end(), dst);
+		_tcsncpy(dst, buf.data(), buf.size());
 		dst[buf.size()] = TEXT('\0');
 
-		DWORD strOffset = sizeof *prcs + (first - CallWindowProc(oldWindowProc, hwnd, EM_LINEINDEX, lineStartPos, 0));
+		DWORD lineStartingPos = Edit_LineIndex(hwnd, lineNumberOfStartPos);
+		DWORD strOffset = first - lineStartingPos;
 		prcs->dwStrLen = bufLen;
 		prcs->dwStrOffset = sizeof *prcs;
-		prcs->dwCompStrLen = selStrLen;
+		prcs->dwCompStrLen = selectedLength;
 		prcs->dwCompStrOffset = strOffset;
-		prcs->dwTargetStrLen = selStrLen;
+		prcs->dwTargetStrLen = selectedLength;
 		prcs->dwTargetStrOffset = strOffset;
+
+		if (selectedLength == 0)
+		{
+			auto himc = ::ImmGetContext(hwnd);
+			if (himc == nullptr)
+			{
+				return 0;
+			}
+			::ImmSetCompositionString(himc, SCS_QUERYRECONVERTSTRING, prcs, prcs->dwSize, nullptr, 0);
+			Edit_SetSel(hwnd, lineStartingPos + prcs->dwCompStrOffset, lineStartingPos + prcs->dwCompStrOffset + prcs->dwCompStrLen);
+			::ImmReleaseContext(hwnd, himc);
+		}
 	}
 	return lr;
 }
@@ -92,9 +110,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 void InitializeReconv()
 {
-	LoadLibrary("k2regexp.dll");
 	if (HWND const hwndTarget = FindEditWindow())
 	{
-		SubclassWindow(hwndTarget, WindowProc);
+		oldWindowProc = SubclassWindow(hwndTarget, WindowProc);
 	}
 }
